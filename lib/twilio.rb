fc87490fe940c1b339102be96ca9a5a4862de133
Michael Ansel <michael.ansel@gmail.com>
2009-06-19 21:01:05 -0400

    First round of tests for Twilio verbs; Adjusted library to facilitate easier testing

--- lib/twilio.rb
+++ lib/twilio.rb
@@ -1,13 +1,19 @@
 module Twilio
   module Verb
     module ClassMethods
-      @attributes = []
-      @allowed_verbs = []
-      attr_accessor :attributes
+      def ClassMethods.extended(other)
+        other.set_defaults
+      end
+
+      def set_defaults
+        @attributes = []
+        @allowed_verbs = []
+        @policy = { :body => :optional, :children => :optional }
+      end
 
       def allowed_verbs(*verbs)
         return @allowed_verbs if verbs == []
-        @allowed_verbs = [] if @allowed_verbs.nil?
+
         verbs.each do |verb|
           @allowed_verbs << verb.to_s.capitalize
         end
@@ -16,11 +22,61 @@ module Twilio
 
       def attributes(*attrs)
         return @attributes if attrs == []
-        @attributes = [] if @attributes.nil?
+
         @attributes = (@attributes + attrs).uniq
         attr_accessor(*@attributes)
         @attributes
       end
+
+      def nesting(policy={})
+        if policy == :prohibited
+          @policy[:body] = policy
+          @policy[:children] = policy
+          return
+        end
+
+        case policy[:body]
+          when :required, :optional
+            @policy[:body] = policy[:body]
+            class_eval "def body; @body ; end
+            def body=(str); @body = str ; end"
+
+          when :prohibited
+            @policy[:body] = policy[:body]
+
+          else
+            raise ArgumentError, "Only :required, :optional, and :prohibited are allowed policies"
+        end
+
+        case policy[:children]
+          when :required, :optional
+            @policy[:children] = policy[:children]
+            class_eval "def children ; @children ; end
+            def children=(ary) ; @children = ary ; end"
+
+          when :prohibited
+            @policy[:children] = policy[:children]
+
+          else
+            raise ArgumentError, "Only :required, :optional, and :prohibited are allowed policies"
+        end
+      end
+
+      def body_required? ; @policy[:body] == :required ; end
+      def body_optional? ; @policy[:body] == :optional ; end
+      def body_prohibited? ; @policy[:body] == :prohibited ; end
+
+      def children_required? ; @policy[:children] == :required ; end
+      def children_optional? ; @policy[:children] == :optional ; end
+      def children_prohibited? ; @policy[:children] == :prohibited ; end
+
+      def verb_name
+        self.to_s.split(/::/)[-1]
+      end
+    end
+
+    def allowed_verbs
+      self.class.allowed_verbs
     end
 
     def attributes
@@ -29,6 +85,18 @@ module Twilio
 
     def allowed?(verb)
       self.class.allowed_verbs.nil? ? false : self.class.allowed_verbs.include?(verb.to_s.capitalize)
+    end
+
+    def body_required?        ; self.class.body_required?       ; end
+    def body_optional?        ; self.class.body_optional?       ; end
+    def body_prohibited?      ; self.class.body_prohibited?     ; end
+
+    def children_required?    ; self.class.children_required?   ; end
+    def children_optional?    ; self.class.children_optional?   ; end
+    def children_prohibited?  ; self.class.children_prohibited? ; end
+
+    def verb_name
+      self.class.verb_name
     end
 
     def initialize(body = nil, params = {})
@@ -57,11 +125,11 @@ module Twilio
       attributes.each {|a| attrs[a] = send(a) unless send(a).nil? } unless attributes.nil?
 
       if @children and @body.nil?
-        b.__send__(self.class.to_s.split(/::/)[-1], attrs) do
+        b.__send__(verb_name, attrs) do
           @children.each {|e|e.to_xml( opts.merge(:skip_instruct => true) )}
         end
       elsif @body and @children == []
-        b.__send__(self.class.to_s.split(/::/)[-1], @body)
+        b.__send__(verb_name, @body)
       else
         raise ArgumentError, "Cannot have children and a body at the same time"
       end
@@ -128,12 +196,14 @@ module Twilio
     extend Twilio::Verb::ClassMethods
     include Twilio::Verb
     attributes :voice, :language, :loop
+    nesting :body => :required, :children => :prohibited
   end
 
   class Play
     extend Twilio::Verb::ClassMethods
     include Twilio::Verb
     attributes :loop
+    nesting :body => :required, :children => :prohibited
   end
 
   class Gather
@@ -141,12 +211,14 @@ module Twilio
     include Twilio::Verb
     attributes :action, :method, :timeout, :finishOnKey, :numDigits
     allowed_verbs :play, :say, :pause
+    nesting :body => :prohibited, :children => :optional
   end
 
   class Record
     extend Twilio::Verb::ClassMethods
     include Twilio::Verb
     attributes :action, :method, :timeout, :finishOnKey, :maxLength, :transcribe, :transcribeCallback
+    nesting :prohibited
   end
 
   class Dial
@@ -154,35 +226,41 @@ module Twilio
     include Twilio::Verb
     attributes :action, :method, :timeout, :hangupOnStar, :timeLimit, :callerId
     allowed_verbs :number
+    nesting :body => :optional, :children => :optional
   end
 
   class Redirect
     extend Twilio::Verb::ClassMethods
     include Twilio::Verb
     attributes :method
+    nesting :body => :required, :children => :prohibited
   end
 
   class Pause
     extend Twilio::Verb::ClassMethods
     include Twilio::Verb
     attributes :length
+    nesting :prohibited
   end
 
   class Hangup
     extend Twilio::Verb::ClassMethods
     include Twilio::Verb
+    nesting :prohibited
   end
 
   class Number
     extend Twilio::Verb::ClassMethods
     include Twilio::Verb
     attributes :sendDigits, :url
+    nesting :body => :required, :children => :prohibited
   end
 
   class Response
     extend Twilio::Verb::ClassMethods
     include Twilio::Verb
     allowed_verbs :say, :play, :gather, :record, :dial, :redirect, :pause, :hangup
+    nesting :body => :prohibited, :children => :optional
   end
 
   module ControllerHooks
--- spec/lib/twilio_spec.rb
+++ spec/lib/twilio_spec.rb
@@ -1,5 +1,201 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe "Twilio" do
-  it "should have a pending spec"
+
+############## Helper Functions ##############
+
+
+def verb_params(verb)
+  raise ArgumentError, "#{verb} is not a valid TwiML verb"  if not Twilio.constants.include?(verb.to_s.downcase.titlecase)
+  verb_class = eval("Twilio::#{verb.to_s.downcase.titlecase}")
+  if verb_class.body_required? or verb_class.body_optional?
+    "String body for #{verb_class.verb_name}"
+  else
+    []
+  end
+end
+
+
+############## Verb Shared Groups ##############
+
+
+describe "a TwiML verb with children", :shared => true do
+  it "should be invalid with no children" if described_class.children_required?
+
+  it "should be invalid with a body" if described_class.children_required? or described_class.body_prohibited?
+
+  it "should produce valid XML with no children" do
+    @verb.should have(0).children
+    @verb.to_xml.should eql("<#{@verb.verb_name}></#{@verb.verb_name}>")
+  end
+
+  it "should contain one child" do
+    child = @verb.allowed_verbs[0]
+    @verb.send(child.downcase.to_sym, *verb_params(child))
+
+    @verb.should have(1).children
+  end
+
+  it "should produce valid XML with one child" do
+    child = @verb.allowed_verbs[0]
+    @verb.send(child.downcase.to_sym, *verb_params(child))
+
+    @verb.to_xml.should eql("<#{@verb.verb_name}>#{@verb.children[0].to_xml}</#{@verb.verb_name}>")
+  end
+
+  it "should contain multiple children" do
+    3.times do |i|
+      child = @verb.allowed_verbs[i % @verb.allowed_verbs.length]
+      @verb.send(child.downcase.to_sym, *verb_params(child))
+    end
+
+    @verb.should have(3).children
+  end
+
+  it "should produce valid XML with multiple children" do
+    3.times do |i|
+      child = @verb.allowed_verbs[i % @verb.allowed_verbs.length]
+      @verb.send(child.downcase.to_sym, *verb_params(child))
+    end
+
+    @verb.to_xml.should eql("<#{@verb.verb_name}>#{@verb.children.collect{|a|a.to_xml}.join('')}</#{@verb.verb_name}>")
+  end
+end
+
+
+describe "a TwiML verb with a body", :shared => true do
+  it_should_behave_like "a TwiML verb with no children"
+  it "should be invalid with no body" if described_class.body_required?
+
+  it "should produce valid XML with no body" do
+    @verb.to_xml.should eql("<#{@verb.verb_name}></#{@verb.verb_name}>")
+  end
+
+  it "should produce valid XML with a body" do
+    @verb.body = "Hello, I <3 Ruby!"
+    @verb.to_xml.should eql("<#{@verb.verb_name}>#{@verb.body.to_xs}</#{@verb.verb_name}>")
+  end
+end
+
+
+describe "a TwiML verb with attributes", :shared => true do
+  it "should produce valid XML with no attributes"
+
+  it "should produce valid XML with attributes"
+
+  it "should have attributes"
+end
+
+
+describe "a TwiML verb with no attributes", :shared => true do
+  it "should not have any attributes" do
+    @verb.should have(0).attributes
+  end
+
+  it "should produce valid XML with no attributes"
+
+  it "should be invalid with attributes"
+end
+
+
+describe "a TwiML verb with no children", :shared => true do
+  it "should be invalid with children" if described_class.children_prohibited?
+
+  it "should produce valid XML with no children" do
+    @verb.to_xml.should eql("<#{@verb.verb_name}></#{@verb.verb_name}>")
+  end
+end
+
+
+############## Verb Specs ##############
+
+
+describe Twilio::Say do
+  before(:each) do
+    @verb = Twilio::Say.new
+  end
+
+  it_should_behave_like "a TwiML verb with attributes"
+  it_should_behave_like "a TwiML verb with a body"
+end
+
+describe Twilio::Play do
+  before(:each) do
+    @verb = Twilio::Play.new
+  end
+
+  it_should_behave_like "a TwiML verb with attributes"
+  it_should_behave_like "a TwiML verb with a body"
+end
+
+describe Twilio::Gather do
+  before(:each) do
+    @verb = Twilio::Gather.new
+  end
+
+  it_should_behave_like "a TwiML verb with attributes"
+  it_should_behave_like "a TwiML verb with children"
+end
+
+describe Twilio::Record do
+  before(:each) do
+    @verb = Twilio::Record.new
+  end
+
+  it_should_behave_like "a TwiML verb with attributes"
+  it_should_behave_like "a TwiML verb with no children"
+end
+
+describe Twilio::Dial do
+  before(:each) do
+    @verb = Twilio::Dial.new
+  end
+
+  it_should_behave_like "a TwiML verb with attributes"
+  it_should_behave_like "a TwiML verb with children"
+  it_should_behave_like "a TwiML verb with a body"
+end
+
+describe Twilio::Redirect do
+  before(:each) do
+    @verb = Twilio::Redirect.new
+  end
+
+  it_should_behave_like "a TwiML verb with attributes"
+  it_should_behave_like "a TwiML verb with a body"
+end
+
+describe Twilio::Pause do
+  before(:each) do
+    @verb = Twilio::Pause.new
+  end
+
+  it_should_behave_like "a TwiML verb with attributes"
+  it_should_behave_like "a TwiML verb with no children"
+end
+
+describe Twilio::Hangup do
+  before(:each) do
+    @verb = Twilio::Hangup.new
+  end
+
+  it_should_behave_like "a TwiML verb with no attributes"
+  it_should_behave_like "a TwiML verb with no children"
+end
+
+describe Twilio::Number do
+  before(:each) do
+    @verb = Twilio::Number.new
+  end
+
+  it_should_behave_like "a TwiML verb with attributes"
+  it_should_behave_like "a TwiML verb with a body"
+end
+
+describe Twilio::Response do
+  before(:each) do
+    @verb = Twilio::Response.new
+  end
+
+  it_should_behave_like "a TwiML verb with no attributes"
+  it_should_behave_like "a TwiML verb with children"
 end
