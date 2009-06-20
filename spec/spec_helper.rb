4a2827fae52405bc693f9514b1ab0ca7b245f004
Michael Ansel <michael.ansel@gmail.com>
2009-06-19 21:44:08 -0400

    Removed MIT License until I have time to decide on licensing; Rewrote spec_helper to run intependent from Rails installation (still uses ActiveSupport); Added spec.opts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
-begin
-  require File.dirname(__FILE__) + '/../../../../spec/spec_helper'
-rescue LoadError
-  puts "You need to install rspec in your base app"
-  exit
-end
+##########################
+RAILS_ENV = 'test'
 
-plugin_spec_dir = File.dirname(__FILE__)
-ActiveRecord::Base.logger = Logger.new(plugin_spec_dir + "/debug.log")
+require 'rubygems'
+require 'active_support'
+require 'spec'
+require 'spec/autorun'
+#require 'spec/rails'
 
+require 'lib/twilio'
